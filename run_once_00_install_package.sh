66c0372d8ae912dd38666a6b27d27a84d4f06d32
Amir Eldor <amir@eldor.dev>
2020-09-07 20:58:53 +0300

    Docker, VS Code, oh-my-zsh

--- run_once_00_install_package.sh
+++ run_once_00_install_package.sh
@@ -2,12 +2,25 @@
 
 set -v
 
-sudo apt update
+# apt repository - vscode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
+# apt repository - docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# apt packages
 sudo apt install -y \
 	build-essential git \
-	neovim \
-	silversearcher-ag
+	neovim code \
+	silversearcher-ag \
+	docker-ce docker-ce-cli containerd.io \
+	zsh
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
@@ -21,8 +34,11 @@ sh /tmp/rustup.sh -y # no prompt
 
 source $HOME/.cargo/env
 
+# some shell prompt
+cargo install starship
+
 # neovim plugins
 nvim +PlugInstall +qa
 
-# some shell prompt
-cargo install starship
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
