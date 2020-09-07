2df2d26562954437fde9225324bb42d9ab709bdf
Amir Eldor <amir@eldor.dev>
2020-09-07 20:16:14 +0300

    Install packages and stuff action!

---
+++ run_once_00_install_package.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -v
+
+sudo apt update
+
+sudo apt install \
+	build-essential git \
+	neovim \
+	silversearcher-ag
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# pyenv
+curl https://pyenv.run | bash
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup.sh
+sh /tmp/rustup.sh -y # no prompt
+
+# neovim plugins
+nvim +PlugInstall +qa
+
+# some shell prompt
+cargo install starship
